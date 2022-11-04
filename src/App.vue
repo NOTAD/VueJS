4c365916e302bd5e7da54278bc49e25786cdd78b
NOTAD <46393687+NOTAD@users.noreply.github.com>
2022-11-04 02:33:53 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import TheHeader from './components/layout/TheHeader.vue'
+</script>
+
+  <main>
+    <TheHeader />
+  </main>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="header">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      The Header
+    
+    </h3>
+  </div>
+  
+</template>
