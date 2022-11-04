f90b8da0f1095974acbea088151d18cd918f944c
NOTAD <boysonyb@gmail.com>
2022-11-04 16:57:28 +0700

    0.0.2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import TheHeader from './components/layout/TheHeader.vue'
+export default {
+  name: 'App'
+}
 </script>
 
-  <main>
-    <TheHeader />
-  </main>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
