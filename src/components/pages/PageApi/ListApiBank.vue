690d934c85c4f075af2a07cd41ada23cac36c9cc
NOTAD <boysonyb@gmail.com>
2022-12-06 14:27:23 +0700

    Update List api

--- src/components/pages/PageApi/ListApiBank.vue
+++ src/components/pages/PageApi/ListApiBank.vue
@@ -1,11 +1,46 @@
 <template>
+  <h3>Danh sách các API hiện tại chúng tôi bán bao gồm:</h3>
+  <div class="list-div-bank">
+    <h2>API Bank</h2>
   <router-link to=/formvietcombank>
     <a-button type="link">Vietcombank</a-button>
   </router-link>
   <router-link to="/formvietinbank">
     <a-button type="link">Vietinbank</a-button>
   </router-link>
-
+  <router-link to="">
+    <a-button type="link">Techcombank mới</a-button>
+  </router-link>
+  <router-link to="">
+    <a-button type="link">BIDV</a-button>
+  </router-link>
+  <router-link to="">
+    <a-button type="link">MB Bank</a-button>
+  </router-link>
+  <router-link to="">
+    <a-button type="link">TP Bank</a-button>
+  </router-link>
+  <router-link to="">
+    <a-button type="link">ACB</a-button>
+  </router-link>
+  </div>
+  <div>
+    <h2>API Ví điện tử</h2>
+    <router-link to="">
+      <a-button type="link">Momo</a-button>
+    </router-link>
+    <router-link to="">
+      <a-button type="link">Zalo Pay</a-button>
+    </router-link>
+    <router-link to="">
+      <a-button type="link">Viettel Pay</a-button>
+    </router-link>
+  </div>
+  <h3>Mỗi api bao gồm: Đăng nhập, kiểm tra lịch sử giao, chuyển tiền</h3>
+  <h3>Đăng nhập và kiểm tra lịch sử giao dịch có thể thực hiện trong 1 lần request.</h3>
+  <h3>Chuyển tiền sẽ cần lấy OTP từ thiết bị thật hoặc máy ảo. Có thể sử dụng Python để adb thiết bị và POST OTP vào webhook</h3>
+  <h2>Mã nguồn của API được viết bằng PHP</h2>
+  <h1>Giá của mỗi API là: 3.500.000đ</h1>
 </template>
 
 <script>
--- src/components/pages/PageApi/LoginVietinbank.vue
+++ src/components/pages/PageApi/LoginVietinbank.vue
@@ -2,8 +2,16 @@
   <div>
     <h3>Vui lòng đăng nhập tài khoản Vietinbank</h3>
     <div>
-      <span>Tài khoản</span>
+      <span>Username</span>
       <input v-model="account"  type="text" name="" id="" placeholder="Nhập tài khoản"/>
+      <br/>
+      <span>Password</span>
+      <input v-model="password" type="text" name="" id="" placeholder="Nhap mat khau"/>
+      <br/>
+      <span>STK</span>
+      <input v-model="stk" type="text" name="" id="" placeholder="Nhap stk">
+      <br/>
+      <button type="button" @click="submit_form">Submit</button>
     </div>
 
 
@@ -12,26 +20,35 @@
 </template>
 
 <script>
-
-
-// import axios from "axios";
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-// axios.defaults.baseURL = 'https://laravel-9.sndn.repl.co/';
-    // const test = () => {
-    //   axios
-    //       .get('/api/loginVTB')
-    //       .then(function (response) {
-    //         console.log(response)
-    //       })
-    //       .catch(function (error) {
-    //         console.log(error)
-    //       })
-    // }
-
+import axios from "axios";
+axios.defaults.baseURL = 'https://beapp.notad.io/';
      export default {
        data() {
          return {
-           account: ""
+           account: "",
+           password: "",
+           stk: ""
+
+         }
+       },
+       methods: {
+         submit_form: function () {
+           // let submit_data = {
+           //   'a': this.account,
+           //   'b': this.password
+           // }
+           let dataAcount = this.account
+           let dataPassword = this.password
+           let dataStk = this.stk
+           console.log(dataPassword, dataAcount)
+           axios
+               .get('api/loginVTB?username=' + dataAcount + '&password=' + dataPassword + '&acc=' + dataStk)
+               .then(function (response) {
+                 console.log(response)
+               })
+               .catch(function (error) {
+                 console.log(error)
+               })
          }
        }
      }
